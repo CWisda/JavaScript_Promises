3880b0d5bf86242eae310cd3cb7c3dd815d80c5b
charles wisda <crwisda@gmail.com>
2024-04-26 11:27:44 -0700

    completed exercise

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ function getList() {
     setTimeout(() => {
       let potentialFail = Math.round(Math.random() * 100) < 10;
       if (potentialFail) {
-        reject({ success: false, message: "Failed to get list of hobbits." });
+        reject(
+          { 
+            success: false, 
+            message: "Failed to get list of hobbits." 
+        }
+        );
       } else {
         resolve(["Bilbo", "Frodo", "Sam", "Merry", "Pippin"]);
       }
@@ -15,10 +20,38 @@ function getList() {
   });
 }
 
+//selecting the DOM ELEMENTS
+const errorParagraph = document.getElementById("error");
+// another way document.quertSelector("#error");
+const unorderedList = document.getElementById("list"); 
+
 // TODO: Handle the resolved or rejected states of the promise
+getList()
+  .then((resolvedValue) => {
+    console.log(resolvedValue);
+    resolvedValue.forEach((hobbit) => {
+      let li = document.createElement("li");
+      li.textContent = hobbit;
+      unorderedList.appendChild(li);
+    });
+    //for loop
+    //for(let i = 0; i < resolvedValue.length; i++) {
+      //let li = document.createElement("li");
+      //li.textContent = resolvedValue[i];
+      //unorderedList.appendChild(li);
+    //}
+
+  })
+  .catch((rejectedValue) => {
+    console.log(rejectedValue);
+    errorParagraph.textContent = rejectedValue.message;
+  })
+  
 
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+
+
 
 // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
